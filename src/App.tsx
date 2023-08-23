d2f19204e6a997d8909fbb0329b1c2c3a8c13c3a
archerax <10404171+archerax@users.noreply.github.com>
2023-08-23 21:32:43 +0100

    Roy G. Biv

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 
 function App() {
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      Hello World
+      <TransformWrapper minScale={0.1} maxScale={2}>
+        <TransformComponent wrapperStyle={{ width: "100%", height: "100%" }}>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "red" }}></div>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "orange" }}></div>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "yellow" }}></div>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "green" }}></div>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "blue" }}></div>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "indigo" }}></div>
+          <div style={{ height: "1000px", width: "1000px", backgroundColor: "violet" }}></div>
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 }
