744300c718a43e4571ece2308874b6c6f0e5eb16
archerax <10404171+archerax@users.noreply.github.com>
2023-08-23 23:41:05 +0100

    dynamic strips

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
+const numStrips = 25;
+const stripWidth = 2500;
+const stripHeight = 1000;
+
+const strips: { y: number; colour: string }[]  = [];
+for (let i = 0; i < numStrips; i++) {
+  strips.push({ y: i * stripHeight, colour: `hsl(${i * 360 / numStrips}, 75%, 50%)` });
+}
 
 function App() {
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <TransformWrapper minScale={0.1} maxScale={2}>
         <TransformComponent wrapperStyle={{ width: "100%", height: "100%" }}>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "red" }}></div>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "orange" }}></div>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "yellow" }}></div>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "green" }}></div>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "blue" }}></div>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "indigo" }}></div>
-          <div style={{ height: "1000px", width: "1000px", backgroundColor: "violet" }}></div>
+          {
+            strips.map(s => (
+              <div style={{ position: "absolute", top: `${s.y}px`, height: `${stripHeight}px`, width: `${stripWidth}px`, backgroundColor: s.colour }}></div>
+            ))
+          }
+          <div style={{ width: `${stripWidth}px`, height: `${numStrips*stripHeight}px` }}></div>
         </TransformComponent>
       </TransformWrapper>
     </div>
